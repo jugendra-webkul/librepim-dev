24fbdadc9411c7dfa5d6cffecc2d0461798c129c
rohit pal <rohitpal.oc563@webkul.in>
2025-12-29 20:17:32 +0530

    test: fix some test issues

---
+++ src/Akeneo/Pim/Permission/Component/Attributes.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Permission\Component;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class Attributes
+{
+    public const VIEW_ITEMS = 'view';
+    public const EDIT_ITEMS = 'edit';
+    public const OWN_ITEMS = 'own';
+}
